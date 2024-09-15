d8dcb52ca1a30b97fe5bd137a368252f392b56c1
Abbas Eren Kılıç <67686692+Aeknasd145@users.noreply.github.com>
2024-09-15 15:45:40 +0300

    Update: version and prefix

--- plexorin.php
+++ plexorin.php
@@ -2,9 +2,8 @@
 /*
 Plugin Name: Blog Yazılarını Sosyal Medyada Otomatik Paylaşın - Plexorin
 Description: WordPress blog içerikleriniz otomatik olarak anında sosyal medya hesaplarınızda paylaşılsın! WordPress to Social.
-Version: 1.0.0
+Version: 1.0.2
 Author: Plexorin
-Plugin URI: https://plexorin.com/tr/
 Author URI: https://plexorin.com/tr/
 License: GPLv2 or later
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
@@ -15,7 +14,7 @@ if (!defined('ABSPATH')) {
 }
 
 // Function to run when the plugin is activated
-function plexorin_activate() {
+function bts_plexorin_activate() {
     // Get all users and categories
     $users = get_users(array('fields' => array('ID')));
     $user_ids = array_map(function($user) { return $user->ID; }, $users);
@@ -23,7 +22,7 @@ function plexorin_activate() {
     $categories = get_categories(array('fields' => 'ids'));
     
     // Save default settings
-    add_option('plexorin_settings', array(
+    add_option('bts_plexorin_settings', array(
         'users' => $user_ids,
         'categories' => $categories,
         'default_title' => '',
@@ -31,7 +30,7 @@ function plexorin_activate() {
         'default_image' => ''
     ));
 }
-register_activation_hook(__FILE__, 'plexorin_activate');
+register_activation_hook(__FILE__, 'bts_plexorin_activate');
 
 // Function to run when the plugin is deactivated
 /*function plexorin_deactivate() {
@@ -40,14 +39,14 @@ register_activation_hook(__FILE__, 'plexorin_activate');
 }
 register_deactivation_hook(__FILE__, 'plexorin_deactivate');*/
 
-function plexorin_admin_enqueue_scripts($hook) {
-    if ($hook != 'toplevel_page_plexorin-settings') {
+function bts_plexorin_admin_enqueue_scripts($hook) {
+    if ($hook != 'toplevel_page_bts-plexorin-settings') {
         return;
     }
     wp_enqueue_media();
     wp_enqueue_script('jquery');
 }
-add_action('admin_enqueue_scripts', 'plexorin_admin_enqueue_scripts');
+add_action('admin_enqueue_scripts', 'bts_plexorin_admin_enqueue_scripts');
 
 // Include admin settings page
 require_once plugin_dir_path(__FILE__) . 'admin/settings-page.php';
